23e5709647394672443983319ebdce020401837d
Todd Roberts <roberts.todd1988@gmail.com>
2024-07-03 01:13:57 -0500

    fix clover field bug hopefully

--- Assets/Scripts/Games/CloverFinder/CloverSpawner.cs
+++ Assets/Scripts/Games/CloverFinder/CloverSpawner.cs
@@ -13,6 +13,7 @@ public class CloverSpawner : MonoBehaviour
     public float initialGroundPlaneHeight = 1.0f; // Initial distance below the camera
 
     public float zOffset = 1f;
+    public float maxXYDifference = 2.0f; // Maximum allowed difference in X and Z coordinates
 
     private void Awake()
     {
@@ -34,15 +35,23 @@ public class CloverSpawner : MonoBehaviour
     void Update()
     {
         // Adjust the ground plane if the camera gets too close
-        if (
-            Camera.main.transform.position.y - groundPlane.transform.position.y
-            < minDistanceFromCamera
-        )
+        if (Camera.main.transform.position.y - groundPlane.transform.position.y < minDistanceFromCamera)
         {
             groundPlane.transform.position = new Vector3(
                 groundPlane.transform.position.x,
                 Camera.main.transform.position.y - minDistanceFromCamera,
                 groundPlane.transform.position.z
+            );
+        }
+
+        // Check the X and Z difference between the ground plane and the camera
+        if (Mathf.Abs(groundPlane.transform.position.x - Camera.main.transform.position.x) > maxXYDifference ||
+            Mathf.Abs(groundPlane.transform.position.z - Camera.main.transform.position.z) > maxXYDifference)
+        {
+            groundPlane.transform.position = new Vector3(
+                Camera.main.transform.position.x,
+                groundPlane.transform.position.y,
+                Camera.main.transform.position.z
             );
         }
     }
@@ -59,9 +68,7 @@ public class CloverSpawner : MonoBehaviour
 
     private void SpawnClovers()
     {
-        int numberOfFourLeafClovers = Mathf.RoundToInt(
-            totalNumberOfClovers * (percentageOfFourLeafClovers / 100)
-        );
+        int numberOfFourLeafClovers = Mathf.RoundToInt(totalNumberOfClovers * (percentageOfFourLeafClovers / 100));
         int numberOfThreeLeafClovers = totalNumberOfClovers - numberOfFourLeafClovers;
 
         for (int i = 0; i < numberOfThreeLeafClovers; i++)
@@ -91,16 +98,18 @@ public class CloverSpawner : MonoBehaviour
 
     private Vector3 GetRandomPointOnPlane()
     {
-        float halfScaleX = groundPlane.transform.localScale.x * 5; // 10 (scale) / 2
-        float halfScaleZ = groundPlane.transform.localScale.z * 5; // 10 (scale) / 2
+        MeshFilter meshFilter = groundPlane.GetComponent<MeshFilter>();
+        Vector3 planeSize = meshFilter.sharedMesh.bounds.size;
+        float halfScaleX = planeSize.x * groundPlane.transform.localScale.x / 2;
+        float halfScaleZ = planeSize.z * groundPlane.transform.localScale.z / 2;
 
         float randomX = Random.Range(-halfScaleX, halfScaleX);
-        float randomZ = Random.Range(-halfScaleZ, halfScaleZ);
+        float randomY = Random.Range(-halfScaleZ, halfScaleZ);
 
         return new Vector3(
             groundPlane.transform.position.x + randomX,
             groundPlane.transform.position.y,
-            groundPlane.transform.position.z + randomZ
+            groundPlane.transform.position.z + randomY
         );
     }
 }
